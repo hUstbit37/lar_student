bd71d5d3e53ae185e68145fbad2fedeb4b836018
DuongDuc <duc.dd108@gmail.com>
2020-03-12 02:42:39 +0700

    add link Login,Register in Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,9 +64,10 @@
         </style>
     </head>
     <body>
-    <p>Page of lar_student</p>
+        <a href="loginQb">Login</a> &nbsp
+        <a href="{{ route('registerQb')}}">Register</a>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            <!-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -78,7 +79,8 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif -->
+            
 
             <div class="content">
                 <div class="title m-b-md">
