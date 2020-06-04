9b2216de91d8eec063a3e8d20f77ea31f122fa1c
DuongDuc <duc.dd108@gmail.com>
2020-06-04 22:22:38 +0700

    add mess session in login view

--- resources/views/KaoPiz/Laravel/auth/login.blade.php
+++ resources/views/KaoPiz/Laravel/auth/login.blade.php
@@ -10,16 +10,25 @@
     <form action="{{ route('getLogin')}}" method="post">
         @csrf
         <label for="">User name</label><br>
-        <input type="text" name='username' placeholder="Username"><br>
+        <input type="text" name='username' placeholder="Username" value="{{ old('username') }}"><br>
         <label for="">Password</label><br>
         <input type="password" name="password" placeholder="Password"><br><br>
-        <button type="submit" class="btn btn-primary">Login</button>
+        <button type=" submit" class="btn btn-primary">Login</button>
     </form>
     <ul>
         @foreach ($errors ->all() as $error)
         <li>{{$error}}</li>
         @endforeach
     </ul>
+    @if ( Session::has('error') )
+    <div class="alert alert-danger alert-dismissible" role="alert">
+        <strong>{{ Session::get('error') }}</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            <span class="sr-only">Close</span>
+        </button>
+    </div>
+    @endif
 </div>
 <p>If you do not have an account! </p>
 <a href="{{route('register')}}">Sign up</a>
